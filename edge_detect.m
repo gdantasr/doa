77d0521db5b10ab39e562daa6b9dc8d9eaf87700
Gabriela Dantas Rocha <gabidantas@poli.ufrj.br>
2019-02-06 15:10:16 -0200

    doa_aevd: Updated to INMETRO data

--- edge_detect.m
+++ edge_detect.m
@@ -36,7 +36,7 @@ function [y_inf, y_sup, fit_inf, fit_sup] = edge_detect (t, t90, tau, Cmat, v, s
     window = indice_min : indice_max; % Intervalo da janela
     tw = t(window);
     Cwindow = Cmat(:, window);
-    threshold = 0.15*max(Cwindow(:));
+    threshold = 0.08*max(Cwindow(:));
     
     G = mat2gray(Cwindow, [threshold max(Cwindow(:))]);
 %     image(100*G)
